79b180e0677e9a96c1ea01e2dc895b185e41472d
thifnmi <tuthin2k@gmail.com>
2021-07-08 21:44:31 +0700

    ubuntu

--- web_sales/main/views.py
+++ web_sales/main/views.py
@@ -7,8 +7,18 @@ from .models import banner,product,users,supplier,orders,order_detail,category,c
 # Create your views here.
 @csrf_exempt
 def index(respone):
+    suppliers = supplier.objects.all()
     categories = category.objects.all()
-    return render(respone, "client/index.html", {"categories" : categories})
+    ao = product.objects.filter(category_id=1).order_by()[:4]
+    quandai = product.objects.filter(category_id=2).order_by()[:4]
+    vaydam = product.objects.filter(category_id=3).order_by()[:4]
+    tuixach = product.objects.filter(category_id=4).order_by()[:4]
+    giay = product.objects.filter(category_id=5).order_by()[:4]
+    vi = product.objects.filter(category_id=6).order_by()[:4]
+    kinh = product.objects.filter(category_id=7).order_by()[:4]
+    vo = product.objects.filter(category_id=8).order_by()[:4]
+    return render(respone, "client/index.html", {"suppliers": suppliers,"categories" : categories,
+    "ao":ao,"quaidai":quandai, "vaydam":vaydam,"tuixach":tuixach,"giay":giay,"vi":vi,"kinh":kinh,"vo":vo})
 
 def shop(respone, id):
     sp_list = product.objects.filter(category_id = str(id))
