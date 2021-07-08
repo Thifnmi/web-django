84f5d5e928b982cb463909ed1fcf9f2e780ab84e
thifnmi <tuthin2k@gmail.com>
2021-07-08 23:09:26 +0700

    commit

--- web_sales/main/views.py
+++ web_sales/main/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator, PageNotAnInteger, InvalidPage
+from django.shortcuts import render
+from django.core.paginator import Paginator
 from django.views.decorators.csrf import csrf_exempt
 from .models import banner,product,users,supplier,orders,order_detail,category,contact,About,image
-
 
 # Create your views here.
 @csrf_exempt
@@ -17,8 +16,8 @@ def index(respone):
     vi = product.objects.filter(category_id=6).order_by()[:4]
     kinh = product.objects.filter(category_id=7).order_by()[:4]
     vo = product.objects.filter(category_id=8).order_by()[:4]
-    return render(respone, "client/index.html", {"suppliers": suppliers,"categories" : categories,
-    "ao":ao,"quaidai":quandai, "vaydam":vaydam,"tuixach":tuixach,"giay":giay,"vi":vi,"kinh":kinh,"vo":vo})
+    return render(respone, "client/index.html", {"suppliers": suppliers,"categories" : categories,"ao":ao,
+    "quaidai":quandai, "vaydam":vaydam,"tuixach":tuixach,"giay":giay,"vi":vi,"kinh":kinh,"vo":vo})
 
 def shop(respone, id):
     sp_list = product.objects.filter(category_id = str(id))
@@ -36,10 +35,8 @@ def login(respone):
 
 def product_detail(respone, id):
     detail = product.objects.get(id=str(id))
-    # print(detail.id)
     categories = category.objects.all()
     product_image = image.objects.filter(product_id = str(detail.id))
-    # print(product_image)
     return render(respone, "client/product-details.html", {"detail": detail,"product_image" : product_image, "categories" : categories})
 
 def contact(respone):
