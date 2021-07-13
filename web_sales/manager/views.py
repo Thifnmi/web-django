4ef067dad6e002e5304109ae9d0f2d368e625fe1
Thifnmi <tuthin2k@gmail.com>
2021-07-13 16:39:34 +0700

    admin page

--- web_sales/manager/views.py
+++ web_sales/manager/views.py
@@ -5,7 +5,14 @@ from main.models import banner,product,users,supplier,orders,order_detail,catego
 # Create your views here.
 @csrf_exempt
 def index(response):
-    return render(response, "page/index.html")
+    categories = category.objects.all()
+    return render(response, "page/dashboard.html", {"categories": categories})
+
+def mailbox(response):
+    return render(response, "page/mailbox.html")
+
+def mailbox_detail(response):
+    return render(response, "page/mail_detail.html")
 
 def login(response):
     return render(response, "page/login.html")
@@ -14,62 +21,62 @@ def logout(response):
     return render(response, "page/login.html")
 
 def user(response):
-    return render(response, "page/users/all-user.html")
+    return render(response, "page/account/index.html")
 
 def adduser(response):
-    return render(response, "page/users/add-user.html")
+    return render(response, "page/account/add.html")
 
 def edituser(response):
-    return render(response, "page/users/edit-user.html")
+    return render(response, "page/account/edit.html")
 
 def userdetail(response):
-    return render(response, "page/users/profile.html")
+    return render(response, "page/account/profile.html")
 
 def categories(response):
-    return render(response, "page/categories/all-category.html")
+    return render(response, "page/category/index.html")
 
 def addcategories(response):
-    return render(response, "page/categories/add-category.html")
+    return render(response, "page/category/add.html")
 
 def editcategories(response):
-    return render(response, "page/categories/edit-category.html")
+    return render(response, "page/category/edit.html")
 
-def categorydetail(response):
-    return render(response, "page/categories/category-detail.html")
+# def categorydetail(response):
+#     return render(response, "page/category/category-detail.html")
 
 def invoices(response):
-    return render(response, "page/invoices/all-invoice.html")
+    return render(response, "page/invoice/index.html")
 
 def addinvoice(response):
-    return render(response, "page/invoices/add-invoice.html")
+    return render(response, "page/invoice/add.html")
 
 def editinvoice(response):
-    return render(response, "page/invoices/edit-invoice.html")
+    return render(response, "page/invoice/edit.html")
 
 def invoicedetial(response):
-    return render(response, "page/invoices/invoice-detail.html")
+    return render(response, "page/invoice/detail.html")
 
 def products(response):
-    return render(response, "page/products/all-product.html")
+    return render(response, "page/product/index.html")
 
 def addproduct(response):
-    return render(response, "page/products/add-product.html")
+    return render(response, "page/product/add.html")
 
 def editproduct(response):
-    return render(response, "page/products/edit-product.html")
+    return render(response, "page/product/edit.html")
 
 def productdetail(response):
-    return render(response, "page/products/product-detail.html")
+    return render(response, "page/product/detail.html")
 
 def supplier(response):
-    return render(response, "page/suppliers/all-supplier.html")
+    return render(response, "page/supplier/index.html")
 
 def addsupplier(response):
-    return render(response, "page/suppliers/add-supplier.html")
+    return render(response, "page/supplier/add.html")
 
 def editsupplier(response):
-    return render(response, "page/suppliers/edit-supplier.html")
+    return render(response, "page/supplier/edit.html")
 
 def supplierdetail(response):
-    return render(response, "page/suppliers/supplier-detail.html")
+    return render(response, "page/supplier/detail.html")
 
